610427834920ee8e22169f8aff2089c2622ffd27
Quocvan <lyquocvan1999@gmail.com>
2024-11-23 19:14:28 +0700

    api lay thong tin anh va thong tin nguoi dang anh

--- backend/src/controllers/imageController.js
+++ backend/src/controllers/imageController.js
@@ -48,3 +48,48 @@ export const getImagesSearchPagination = async (req, res) => {
     timestamp: new Date().toISOString(),
   });
 };
+
+// /lay-thong-tin-hinh-anh/:id
+export const getImageInfoById = async (req, res) => {
+  const { id } = req.params;
+
+  console.log("CHECK ID", id);
+
+  const checkImage = await prisma.hinh_anh.findUnique({
+    where: {
+      hinh_id: Number(id),
+    },
+    select: {
+      hinh_id: true,
+      ten_hinh: true,
+      duong_dan: true,
+      mo_ta: true,
+      nguoi_dung: {
+        select: {
+          nguoi_dung_id: true,
+          email: true,
+          ho_ten: true,
+          tuoi: true,
+          anh_dai_dien: true,
+        },
+      },
+    },
+  });
+
+  if (!checkImage) {
+    return res.status(404).json({
+      message: "Hình ảnh không tồn tại",
+      statusCode: 404,
+      timestamp: new Date().toISOString(),
+    });
+  }
+
+  return res.status(200).json({
+    message: "Lấy thông tin hình ảnh thành công",
+    content: {
+      data: checkImage,
+    },
+    statusCode: 200,
+    timestamp: new Date().toISOString(),
+  });
+};
--- backend/src/routes/imageRoutes.js
+++ backend/src/routes/imageRoutes.js
@@ -1,6 +1,9 @@
 import express from "express";
 import { tryCatch } from "../config/trycatch.js";
-import { getImagesSearchPagination } from "../controllers/imageController.js";
+import {
+  getImageInfoById,
+  getImagesSearchPagination,
+} from "../controllers/imageController.js";
 
 const imageRoutes = express.Router();
 
@@ -8,5 +11,6 @@ imageRoutes.get(
   "/lay-danh-sach-hinh-anh-tim-kiem-phan-trang",
   tryCatch(getImagesSearchPagination)
 );
+imageRoutes.get("/lay-thong-tin-hinh-anh/:id", tryCatch(getImageInfoById));
 
 export default imageRoutes;
